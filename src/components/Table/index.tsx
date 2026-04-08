8ab9f96b82e2fe49f870d67f86ce0db193248119
chris_virtuascript <chris@virtuascript.com>
2026-04-09 00:59:37 +0800

    Modifications for Table component to fix data handling and rendering issue

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import MuiTable, { TableProps } from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableContainer from "@mui/material/TableContainer";
@@ -8,40 +10,82 @@ import TablePagination, {
 } from "@mui/material/TablePagination";
 import React, { Children, cloneElement } from "react";
 
+export type CustomTableRowProps = {
+  id: string;
+};
+
 type TableComponentProps = TableProps & {
-  tableRows?: Array<object>;
+  tableRows?: Array<CustomTableRowProps>;
   pagination?: TablePaginationProps;
-  otherProps: any;
+  onTableRowClick?: (obj: object) => void | undefined;
+  otherProps?: any;
 };
 
 export default function Table(props: TableComponentProps) {
-  const { children, tableRows = [], pagination, ...otherProps } = props;
+  const {
+    children,
+    tableRows = [],
+    pagination,
+    onTableRowClick,
+    ...otherProps
+  } = props;
   const tableHeaders: Array<React.ReactNode> = [];
   const tableContents: Array<React.ReactNode> = [];
+
+  const handleRowClicked = (row: object) => {
+    if (onTableRowClick) {
+      onTableRowClick(row);
+    }
+  };
 
   const childrenArray = Children.toArray(children);
   if (childrenArray?.length) {
     for (let index = 0; index < childrenArray.length; index++) {
       const childElement: any = childrenArray[index];
-      tableHeaders.push(
-        cloneElement(childElement, {
-          key: `table-header-${index}`,
-          index,
-          isHeader: true,
-        }),
-      );
+      const clonedHeaderElement = cloneElement(childElement, {
+        key: `table-header-${index}`,
+        index,
+        _isheader: true,
+      });
+      console.log(clonedHeaderElement.props);
+      tableHeaders.push(clonedHeaderElement);
     }
-    if (tableRows) {
-      for (let index = 0; index < tableRows.length; index++) {
-        const rowElement: any = tableRows[index];
-        tableContents.push(
-          cloneElement(rowElement, {
-            key: `table-row-${index}`,
-            index,
-            rowData: rowElement,
-          }),
-        );
+  }
+
+  if (childrenArray.length && tableRows) {
+    for (let index = 0; index < tableRows.length; index++) {
+      const rowElement: any = tableRows[index];
+      const tableColumns = [];
+      for (let j = 0; j < childrenArray.length; j++) {
+        const childRowElement: any = childrenArray[j];
+
+        const clonedTableRow = cloneElement(childRowElement, {
+          key: `table-row-${index}-column-${j}`,
+          index,
+          _rowdata: rowElement,
+        });
+        console.log(clonedTableRow.props);
+        tableColumns.push(clonedTableRow);
       }
+
+      tableContents.push(
+        <TableRow
+          key={
+            "id" in tableRows[index]
+              ? tableRows[index].id
+              : "table-entries-" + index
+          }
+          className="cursor-pointer"
+          hover
+          tabIndex={-1}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleRowClicked(tableRows[index]);
+          }}
+        >
+          {tableColumns}
+        </TableRow>,
+      );
     }
   }
 
