db2b936e20ab7bfb852768bef89e8eda2dde2cb5
chris_virtuascript <chris@virtuascript.com>
2026-05-07 14:07:57 +0800

    Added courses api

---
+++ src/app/(pages)/(admin)/academics/courses/CoursesApi.ts
@@ -0,0 +1,32 @@
+import { apiService } from "@/src/store/apiService";
+import { CourseType } from "@/src/types";
+
+export type GetCoursesApiResponse = CourseType[];
+export type GetCoursesApiArg = { track: string };
+
+const coursesApi = apiService.injectEndpoints({
+    /**
+     * endpoints
+     * @param {function} build - The build function provided by apiService
+     * @returns {object} - The endpoints object
+     * @description The endpoints object
+     */
+    endpoints: (build) => ({
+        getCourses: build.query<GetCoursesApiResponse, GetCoursesApiArg>({
+            query: (params) => ({
+                url: "/api/academics/courses",
+                params,
+            }),
+            providesTags: ["courses_list"],
+        }),
+    }),
+    overrideExisting: false,
+});
+
+export default coursesApi;
+
+export const { useGetCoursesQuery } = coursesApi;
+
+export type CoursesApiType = {
+    [coursesApi.reducerPath]: ReturnType<typeof coursesApi.reducer>;
+};
