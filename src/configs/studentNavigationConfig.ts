8c22ab9b9d6a7d40e6b2835af5a5aef2c203fc4f
chris_virtuascript <chris@virtuascript.com>
2026-05-07 14:24:53 +0800

    Added student and updated admin config

---
+++ src/configs/studentNavigationConfig.ts
@@ -0,0 +1,14 @@
+/**
+ * Navigation configuration for the application
+ * for students
+ * @type {Array}
+ */
+export const studentNavigationConfig = [
+  {
+    id: "dashboard",
+    title: "Dasboard",
+    type: "item",
+    url: "/student/dashboard",
+    icon: "dashboard",
+  },
+];
