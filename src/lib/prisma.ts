6da15320e40d94eeb15b6770c9a54248d889d5bc
chris_virtuascript <chris@virtuascript.com>
2026-05-16 13:27:57 +0800

    Optimize prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,26 +1,34 @@
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/prisma";
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is not defined");
+}
+
+const adapter = new PrismaPg({ connectionString });
+
 /** 
  * Define a global prisma variable so that it can be accessed from anywhere
- */
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+*/
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
 /**
  * Define the prisma client
  * If the prisma client is not defined, create a new instance and set it to the global variable
  * @returns {PrismaClient}
- */
-const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
-    }),
-  });
-
-globalForPrisma.prisma = prisma;
+*/
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  adapter,
+  omit: {
+    user: { password: true },
+  },
+});
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
