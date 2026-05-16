c07799eb6e3afec5e4faadb2150d39e97de714ca
chris_virtuascript <chris@virtuascript.com>
2026-05-16 13:28:50 +0800

    Optimize next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false, // Disables heavy browser-side source maps
+  experimental: {
+    serverSourceMaps: false,          // Disables heavy server-side source maps
+    workerThreads: false,               // Disables heavy worker threads
+    cpus: 1,                           // Sets the number of CPUs to use
+  },
 };
 
 export default nextConfig;
